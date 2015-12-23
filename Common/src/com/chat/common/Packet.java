449be14b3e0099201e25aa45589636056fc99ff8
dumrelu <dum.relu@gmail.com>
2015-12-23 18:40:51 +0200

    Packet and PacketTYpe.

---
+++ Common/src/com/chat/common/Packet.java
@@ -0,0 +1,13 @@
+package com.chat.common;
+
+/**
+ *  The interface the client and server use to 
+ * send and receive data.
+ * 
+ * @author Relu
+ */
+public interface Packet 
+{
+    // Returns the type of the packet.
+    public PacketType getType();
+}
---
+++ Common/src/com/chat/common/PacketType.java
@@ -0,0 +1,22 @@
+package com.chat.common;
+
+/**
+ *  Types of packets that are used to communicate between
+ * server and client.
+ * 
+ * @author Relu
+ */
+public enum PacketType 
+{
+    COMMAND_CONNECT,    //Connect to server command.
+    COMMAND_LIST,       //Get all the connected users.
+    COMMAND_MSG,        //Send a private or broadcast message.
+    COMMAND_UPDATE,     //Update the user data.
+    COMMAND_QUIT,       //Disconnect from the server.
+    
+    MESSAGE_CONNECT,    //A user has connected to the server.
+    MESSAGE_DISCONNECT, //A user has disconnected from the server.
+    MESSAGE_UPDATE,     //A user changed its data.
+    MESSAGE_CHAT,       //A user sent a chat message(private or broadcast).
+    MESSAGE_ERROR,      //An error has occured when executing a command.
+}
